515e2ff77800a3fe12285157a5cc94b646aa86d5
Abhi <suriabhinav1997@gmail.com>
2016-01-24 07:44:11 -0500

    fixes

--- server.js
+++ server.js
@@ -111,6 +111,6 @@ io.on('connection', (socket) => {
     io.emit('ADD_VIEW', currentViews[currentViews.length - 1]);
   });
   socket.on('UPD', (data) => {
-    io.emit('ADD_VIEW', {type: data.choice, rotate: data.rotate, zoom: data.zoom, x: data.x, y: data.y, z: data.z, equation: data.equation })
+    io.emit('ADD_VIEW', {type: data.choice, rotate: data.rotate, zoom: data.zoom, x: data.x, y: data.y, z: data.z, equation: data.equation, rotate_rate: 0 })
   })
 });
